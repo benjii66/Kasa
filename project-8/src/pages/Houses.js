e4c5b74b3153492746def7dd1a0cbde4bc881ad4
B.N.J <benjamin51.simon@gmail.com>
2023-12-13 10:50:02 +0100

    before the design I set some comments

--- project-8/src/pages/Houses.js
+++ project-8/src/pages/Houses.js
@@ -12,45 +12,66 @@ import {   Rate   } from "../components/common/StarRating"
 
 export const Houses = () => {
 
+  //get the navigation parameters and the URL too
   const params = useParams();
   const navigate = useNavigate(); 
 
-  const [house, setchosenAppart] = useState();
+  //state to stock the chosen house
+  const [house, setHousePicked] = useState();
     
     useEffect(() => {
+    //async function to get the data from the chosen house
       const getData = async () => {
 
-        const houseResponse = await axios.get("/logements.json");
-        const houseChosen = houseResponse.data.find(({ id }) => id === params.id);
-        houseResponse.data.map(() => setchosenAppart(houseChosen));
-        if (houseChosen === undefined) navigate("/404", { state: { message: "Can't get data" } }); 
-        
+        try {
+          //request to fetch the datas from the JSON
+          const houseResponse = await axios.get("/logements.json");
+          //find the house by the ID
+          const houseChosen = houseResponse.data.find(({ id }) => id === params.id);
+          //update the state with the house datas
+          setHousePicked(houseChosen);
+
+          //goto error page if the house doesn't exist
+          if (houseChosen === undefined) {
+            navigate("/404", { state: { message: "Impossible de récupérer les données" } });
+          }
+        } catch (error) {
+          console.error(error);
+        }
       };
+      //call the function to have the datas when the composant is ready
       getData();
       
-    }); 
+      //ensure the function is called when the composant is ready
+    }, [params.id, navigate]); 
+
+    //extract the house datas (if exist)
     const housePics = house && house.pictures;
     const tags = house && house.tags;
     const equipments = house && house.equipments;
     const allEquipments =
       house &&
-      equipments.map((item, index) => (
-        <li key={index}>
+      equipments.map((item, equipmentIndex) => (
+        <li key={equipmentIndex}>
           {item}
         </li>
       ));
 
 
     return (
+      //condition if the datas are available
       house && (
         <div key ={params.id} >
+          {/* display the carrousel with the house images */}
           <Carousel slides ={housePics} />
           <section >
             <div>
               <div>
+                {/* title and location of the house */}
                 <h1>{house.title}</h1>
                 <h3>{house.location}</h3>
-                  <div className="tag">              
+                  <div className="tag">  
+                  {/* his tags */}
                     {tags.map((tag) => (
                       <Tags key={tag} tag={tag} />
                     ))}              
@@ -59,17 +80,20 @@ export const Houses = () => {
             </div>
             <div>
               <div>
+                {/* display hosts informations  */}
                 <Host
                   hostName = {house.host.name}
                   hostPic = {house.host.picture}
                 />
               </div>
               <div>
+                {/* display the rate  */}
                 <Rate score={house.rating}/>
               </div>
             </div>
           </section>
           <div>
+            {/* display the description and the equipments through an accordion  */}
           <Collapse title="Description" content={house.description} />
           <Collapse title="Equipements" content={allEquipments} />        
           </div>
